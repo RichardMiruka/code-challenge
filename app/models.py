71acade96ffb988f47fd3503e0d5c9865a636954
richard miruka onsare <richardonsare15@gmail.com>
2023-07-05 19:52:26 +0300

    added hero_power = db.Table

--- app/models.py
+++ app/models.py
@@ -2,6 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+hero_power = db.Table(
+    'hero_powers',
+    db.Column('hero_id', db.Integer, db.ForeignKey('heroes.id'), primary_key=True),
+    db.Column('power_id', db.Integer, db.ForeignKey('powers.id'), primary_key=True),
+    db.Column("strength", db.String),
+    db.Column("created_at", db.DateTime, server_default=db.func.now()),
+    db.Column("updated_at", db.DateTime, onupdate=db.func.now())
+)
+
 class Hero(db.Model):
     __tablename__ = 'hero'
 
