0358568e601ff1b97f2ce3d15e8a080d7d0b4bec
richard miruka onsare <richardonsare15@gmail.com>
2023-07-05 19:39:24 +0300

    added two more classes

--- app/app.py
+++ app/app.py
@@ -79,7 +79,68 @@ class HeroesById(Resource):
           
 api.add_resource(HeroesById, '/heroes:<int:id>')
 
-                
+#GET(/Powers)
+
+class Powers(Resource):
+    def get(self):
+        # powers = [power.to_dict() for power in Power.query.all()]
+        powers = []
+        for power in Power.query.all():
+            power_data={
+                "id": power.id,
+                "name": power.name,
+                "description": power.description,
+                "created_at":power.created_at,
+                "hero_ps": [
+                    {
+                        "strength": hero_p.strength,
+                        "hero_id": hero_p.hero_id
+                    }
+                    for hero_p in power.hero_ps
+                ]
+
+            }
+            powers.append(power_data)
+        return make_response(jsonify(powers),200)
+    
+api.add_resource(Powers, '/powers') 
+
+ # GET (/powers/:id)
+  
+class PowersById(Resource):
+    def get(self, id):
+        powers = []
+        power = Power.query.filter_by(id=id).first()
+        if power:
+            power_data = {
+                "id": power.id,
+                "name": power.name,
+                "description": power.description,
+                "created_at": power.created_at,
+                "hero_ps": [
+                    {
+                        "strength": hero_p.strength,
+                        "hero_id": hero_p.hero_id
+                    }
+                    for hero_p in power.hero_ps
+                ]
+            }
+            powers.append(power_data)
+            return make_response(jsonify(powers), 200)
+        else:
+               response_dict={
+                "error": "Power not found"
+               }
+               response = make_response(
+                jsonify(response_dict), 404
+               )
+               return response
+    
+      
+    
+    
+    
+                    
             
 if __name__ == '__main__':
     app.run(port=5555)
